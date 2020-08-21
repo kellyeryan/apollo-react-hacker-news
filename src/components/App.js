613bfa09946436897db515741620a89c3b048b62
kellyeryan <kellyeryan@gmail.com>
2020-08-21 10:54:46 -0400

    Finish Creating Links Section

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import CreateLink from './CreateLink'
+
+class App extends Component {
+  render() {
+    return <CreateLink />
+  }
+}
+
+export default App
+
