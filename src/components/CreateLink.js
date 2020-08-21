8c503743632d0ab3b547f7133c6d465cf6cde5d2
kellyeryan <kellyeryan@gmail.com>
2020-08-21 14:45:12 -0400

    Finish Hackernews React Apollo Tutorial

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
+import { FEED_QUERY } from './LinkList'
+import { LINKS_PER_PAGE } from '../constants'
 
-class CreateLink extends Component {
-  state = {
-    description: '',
-    url: '',
-  }
-
-  render() {
-
-    const POST_MUTATION = gql `
+export const POST_MUTATION = gql `
         mutation PostMutation($description: String!, $url: String!) {
             post(description: $description, url: $url) {
                 id
@@ -19,6 +13,14 @@ class CreateLink extends Component {
                 description
             }
         }`
+
+class CreateLink extends Component {
+  state = {
+    description: '',
+    url: '',
+  }
+
+  render() {
 
     const { description, url } = this.state
     return (
@@ -39,8 +41,27 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
-            {postMutation => <button onClick={postMutation}>Submit</button>}
+        <Mutation
+          mutation={POST_MUTATION}
+          variables={{ description, url }}
+          onCompleted={() => this.props.history.push('/new/1')}
+          update={(store, { data: { post } }) => {
+            const first = LINKS_PER_PAGE
+            const skip = 0
+            const orderBy = 'createdAt_DESC'
+            const data = store.readQuery({
+              query: FEED_QUERY,
+              variables: { first, skip, orderBy }
+            })
+            data.feed.links.unshift(post)
+            store.writeQuery({
+              query: FEED_QUERY,
+              data,
+              variables: { first, skip, orderBy }
+            })
+          }}
+        >
+          {postMutation => <button onClick={postMutation}>Submit</button>}
         </Mutation>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,31 +2,62 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-import * as serviceWorker from './serviceWorker';
-
-// 1
+import * as serviceWorker from './serviceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
-
-// 2
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: 'http://localhost:4000',
 })
 
-// 3
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
 })
 
-// 4
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink),
 )
-serviceWorker.unregister();
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+)
+serviceWorker.unregister()
